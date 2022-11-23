ae53efbb7ab6d117cea8aee1da7259bdfef0a9a1
Ashutosh9424 <ashutoshpandey9727@gmail.com>
2022-11-23 23:31:32 +0530

    image-commit

--- LoginWin.py
+++ LoginWin.py
@@ -1,8 +1,11 @@
-from tkinter import*
+from tkinter import *
 from tkinter import messagebox
 import mysql.connector
 from adminmainwin import adminstartwin
+# from checkimage import label1
 from usermainwin import userstartwin
+from PIL import ImageTk, Image
+from PIL import Image, ImageTk
 
 
 def adminLogin():
@@ -15,27 +18,29 @@ def adminLogin():
     )
     if con:
         messagebox.showinfo("Success", "Connection Done")
-        #input
+        # input
         inname = enname.get()
         inpass = enpass.get()
-        #database(fetch)
+        # database(fetch)
         qry = "SELECT *FROM users where name=%s and password=%s and usertype='admin'"
         tpdata = (inname, inpass)
         cur = con.cursor()
-        cur.execute(qry, tpdata) ### cur.table id name password
-                                             #101 Sumit verma 6666
+        cur.execute(qry, tpdata)  ### cur.table id name password
+        # 101 Sumit verma 6666
         data = cur.fetchone()
-        if data!=None:
+        if data != None:
             mw.wm_withdraw()
             adminstartwin()
 
         else:
-            messagebox.showinfo("info","login failed")
+            messagebox.showinfo("info", "login failed")
 
     else:
-        messagebox.showinfo("failed","connection failed")
+        messagebox.showinfo("failed", "connection failed")
+
+
 def userLogin():
-    con=mysql.connector.connect(
+    con = mysql.connector.connect(
         host="localhost",
         user="root",
         password="soley2000",
@@ -43,43 +48,59 @@ def userLogin():
 
     )
     if con:
-        messagebox.showinfo("Success","Connection Done")
-        #input
-        inname=enname.get()
-        inpass=enpass.get()
-        #database(fetch)
-        qry="SELECT *FROM users where name=%s and password=%s and usertype='user'"
-        tpdata=(inname,inpass)
-        cur=con.cursor()
-        cur.execute(qry,tpdata) ### cur.table id name password
-                                             #101 Sumit verma 6666
-        data=cur.fetchone()
-        if data!=None:
+        messagebox.showinfo("Success", "Connection Done")
+        # input
+        inname = enname.get()
+        inpass = enpass.get()
+        # database(fetch)
+        qry = "SELECT *FROM users where name=%s and password=%s and usertype='user'"
+        tpdata = (inname, inpass)
+        cur = con.cursor()
+        cur.execute(qry, tpdata)  ### cur.table id name password
+        # 101 Sumit verma 6666
+        data = cur.fetchone()
+        if data != None:
             mw.wm_withdraw()
             userstartwin()
 
         else:
-            messagebox.showinfo("info","login failed")
+            messagebox.showinfo("info", "login failed")
 
     else:
-        messagebox.showinfo("failed","connection failed")
-mw=Tk()
-mw.geometry("300x200")
-lbname=Label(mw,text="Enter Name     ")
-lbpass=Label(mw,text="Enter Password    ")
-enname=Entry(mw)
-enpass=Entry(mw,show="*")
+        messagebox.showinfo("failed", "connection failed")
 
-adminLoginBtn=Button(text="AdminLogin",command=adminLogin)
-userLoginBtn=Button(text="UserLogin",command=userLogin)
 
-lbname.grid(row=0,column=0)
+mw = Tk()
+mw.geometry("800x400")
+lbname = Label(mw, text="Enter Name     ")
+lbpass = Label(mw, text="Enter Password    ")
+enname = Entry(mw)
+enpass = Entry(mw, show="*")
 
-enname.grid(row=0,column=1)
-lbpass.grid(row=1,column=0)
+adminLoginBtn = Button(text="AdminLogin", command=adminLogin)
+userLoginBtn = Button(text="UserLogin", command=userLogin)
 
-enpass.grid(row=1,column=1)
-adminLoginBtn.grid(row=4, column=0)
-userLoginBtn.grid(row=4, column=1)
+# lbname.grid(row=0,column=0)
+#
+# enname.grid(row=0,column=1)
+# lbpass.grid(row=1,column=0)
+#
+# enpass.grid(row=1,column=1)
+# adminLoginBtn.grid(row=4, column=0)
+# userLoginBtn.grid(row=4, column=1)
+
+# lbname.place(x=80, y=130)
+# enname.place(x=200,y=130)
+
+image = Image.open("second.jpg")
+
+# Resize the image using resize() method
+resize_image = image.resize((150, 150))
+
+img = ImageTk.PhotoImage(resize_image)
+
+# create label and add resize image
+Label(image=img).place(x=50, y=50)
+
 
 mw.mainloop()
